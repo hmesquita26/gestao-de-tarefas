a6990d8fc1a5f660dd0146fd1fff1c46e782a130
Heytor Mesquita <heytormesquita@gmail.com>
2023-06-07 14:29:58 -0300

    adiciona base do projeto

---
+++ gestao-de-tarefas-back/src/main/java/com/sistema/gestaodetarefasback/GestaoDeTarefasBackApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.gestaodetarefasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeTarefasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeTarefasBackApplication.class, args);
+	}
+
+}
---
+++ gestao-de-tarefas-back/src/test/java/com/sistema/gestaodetarefasback/GestaoDeTarefasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.gestaodetarefasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeTarefasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
