a8b9654fa4ad0d97e70a0660d3d1ebbfad840694
Heytor Mesquita <heytormesquita@gmail.com>
2023-06-11 15:47:13 -0300

    adiciona listagem por agrupamento de estado no repository

--- gestao-de-tarefas-back/src/main/java/com/sistema/gestaodetarefasback/repository/TarefaRepository.java
+++ gestao-de-tarefas-back/src/main/java/com/sistema/gestaodetarefasback/repository/TarefaRepository.java
@@ -1,6 +1,10 @@
 package com.sistema.gestaodetarefasback.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sistema.gestaodetarefasback.domain.Tarefa;
@@ -8,4 +12,15 @@ import com.sistema.gestaodetarefasback.domain.Tarefa;
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Integer>{
 	
+	@Query("select t from tarefa t where t.estado = 0")
+	Optional<List<Tarefa>> findByTarefasNaoIniciadas();
+	
+	@Query("select t from tarefa t where t.estado = 1")
+	Optional<List<Tarefa>> findByTarefasEmProgresso();
+	
+	@Query("select t from tarefa t where t.estado = 2")
+	Optional<List<Tarefa>> findByTarefasFinalizadas();
+	
+	@Query("select t from tarefa t where t.estado = 3")
+	Optional<List<Tarefa>> findByTarefasArquivadas();
 }
